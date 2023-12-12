ffcdcb05782183a330e237e3578b881dbf2f172f
Cristian <cristianbm365@gmail.com>
2023-12-11 23:03:38 -0500

    Create combos helper

--- Shopping/Shopping/Program.cs
+++ Shopping/Shopping/Program.cs
@@ -10,18 +10,18 @@ builder.Services.AddControllersWithViews();
 
 builder.Services.AddDbContext<DataContext>(o => // acá, esto viene de CountriesController
 {
-    o.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+	o.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
 }); // significa que cualquier clase que llame al datacontext trae lo anterior, una conexión con el sql server y está basado con el GetConnectionString
 
 //TODO: Make strangest password
 builder.Services.AddIdentity<User, IdentityRole>(cfg =>
 {
-    cfg.User.RequireUniqueEmail = true;
-    cfg.Password.RequireDigit = false;
-    cfg.Password.RequiredUniqueChars = 0;
-    cfg.Password.RequireLowercase = false;
-    cfg.Password.RequireNonAlphanumeric = false;
-    cfg.Password.RequireUppercase = false;
+	cfg.User.RequireUniqueEmail = true;
+	cfg.Password.RequireDigit = false;
+	cfg.Password.RequiredUniqueChars = 0;
+	cfg.Password.RequireLowercase = false;
+	cfg.Password.RequireNonAlphanumeric = false;
+	cfg.Password.RequireUppercase = false;
 }).AddEntityFrameworkStores<DataContext>();
 
 builder.Services.ConfigureApplicationCookie(options =>
@@ -32,6 +32,7 @@ builder.Services.ConfigureApplicationCookie(options =>
 
 builder.Services.AddTransient<SeedDb>();
 builder.Services.AddScoped<IUserHelper, UserHelper>(); // la gran mayoría son scoped porque lo crea cuando lo necesita y lo destruye cuando lo deja de usar
+builder.Services.AddScoped<ICombosHelper, CombosHelper>();
 builder.Services.AddRazorPages().AddRazorRuntimeCompilation();
 
 var app = builder.Build();
@@ -39,20 +40,20 @@ SeedData();
 
 void SeedData()
 {
-    IServiceScopeFactory? scopedFactory = app.Services.GetService<IServiceScopeFactory>();
+	IServiceScopeFactory? scopedFactory = app.Services.GetService<IServiceScopeFactory>();
 
-    using (IServiceScope? scope = scopedFactory.CreateScope())
-    {
-        SeedDb? service = scope.ServiceProvider.GetService<SeedDb>();
-        service.SeedAsync().Wait();
-    }
+	using (IServiceScope? scope = scopedFactory.CreateScope())
+	{
+		SeedDb? service = scope.ServiceProvider.GetService<SeedDb>();
+		service.SeedAsync().Wait();
+	}
 
 }
 
 if (!app.Environment.IsDevelopment())
 {
-    app.UseExceptionHandler("/Home/Error");
-    app.UseHsts();
+	app.UseExceptionHandler("/Home/Error");
+	app.UseHsts();
 }
 
 app.UseStatusCodePagesWithReExecute("/error/{0}");
@@ -63,7 +64,7 @@ app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+	name: "default",
+	pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
