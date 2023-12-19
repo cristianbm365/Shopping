47ec05cf07a00a2e8641ddfffedc0ac5625ff768
Cristian <cristianbm365@gmail.com>
2023-12-19 18:10:07 -0500

    Add products to cart

--- Shopping/Shopping/Data/DataContext.cs
+++ Shopping/Shopping/Data/DataContext.cs
@@ -22,6 +22,9 @@ namespace Shopping.Data
         public DbSet<ProductImage> ProductImages { get; set; }
 
         public DbSet<State> States { get; set; }
+
+        public DbSet<TemporalSale> TemporalSales { get; set; }
+        
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
